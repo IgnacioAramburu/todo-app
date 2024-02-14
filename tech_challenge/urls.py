420c79fa5ac14e51e06fd04df4e2de163075e762
Ignacio Matias Aramburu <ignaciomatiasaramburudeveloper@gmail.com>
2024-02-14 10:03:27 -0300

    remove ui

--- tech_challenge/urls.py
+++ tech_challenge/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('',include('todo.urls')),
     path('api/',include('todo_api.urls')),
     path('accounts/',include('allauth.urls')),
     path('admin/', admin.site.urls),
